d40d38c31072f3925e3316bb8f4a2c16dc1e85d3
johnykes <johnslymusic@gmail.com>
2023-03-11 19:38:41 +0200

    private profile page and router auth check

---
+++ src/views/ProfilePage/ProfilePage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="d-flex flex-column flex-grow-1">
+    <div class="d-flex flex-fill align-items-center container">
+      <div class="row w-100">
+        <div class="col-12 col-md-8 mx-auto">
+          <div class="card shadow-sm rounded p-4 border-0">
+            <div class="card-body text-center">
+              <h3>PRIVATE</h3>
+              <p>auth: {{ isLoggedIn }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfilePage",
+  computed: {
+    isLoggedIn() {
+      return this.$erdjs.getDapp().getLoginInfoStore().isLoggedIn;
+    },
+  },
+  methods: {
+    async newTransaction() {
+      const tx = {
+        value: "100000000000000000",
+        data: "ping",
+        receiver:
+          "erd1g5dqap37a650g564nsehjwxd9m3pzgxla83pcd3w7f5s8lgxq9eq3g884u",
+      };
+      this.$erdjs.dapp.sendTransaction(tx).then(({ sessionId, error }) => {
+        if (error) {
+          alert(error);
+        }
+      });
+    },
+  },
+};
+</script>
